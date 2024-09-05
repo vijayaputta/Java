dadb922fe3be56e8388094bcd85591c2c8f69cf3
vijayaputta <151710074+vijayaputta@users.noreply.github.com>
2024-09-05 12:45:25 +0530

    Create Foreachdemo.java

---
+++ Foreachdemo.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class Foreachdemo
+{
+public static void main(String args[])
+{  
+   int arr[]={10,20,30,40,50};
+   {
+ System.out.println("Array elements are:");
+ for(int i:arr)
+ {
+   System.out.println(i+"");
+ }
+      }
+}
+}
