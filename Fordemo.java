1ec780dfe72c34d52964fe6a9f821c216636f033
vijayaputta <151710074+vijayaputta@users.noreply.github.com>
2024-09-06 14:23:08 +0530

    Create Fordemo.java

---
+++ Fordemo.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class Fordemo
+{
+    public static void main (String args[])
+    {
+        for(int i=0;i<100;i++)
+        {
+            System.out.println("Hello World");
+        }
+    }
+}                                                                              
